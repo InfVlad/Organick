eb6666cfb8c9dd54d4f0744074a9fd187c8fa474
SpicyArepa <infantevladimir@gmail.com>
2023-04-06 11:03:14 -0400

    migrated to app dir, added some tailwind config

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import '../styles/globals.css';
+// import { Radio_Canada } from 'next/font/google';
+
+export const metadata = {
+  title: 'Organick',
+  description: 'Best place to buy organic food',
+  // keywords: ['Frontend', 'Fullstack', 'Developer', 'Next.js'],
+  // openGraph: {
+  //   title: 'Vladimir Infante - Portfolio',
+  //   description: "Vladimir Infante's Developer Portfolio built with NextJs 13.2",
+  //   images: [
+  //     {
+  //       url: 'https://vlad-infante-portfolio.vercel.app/assets/portfolio-home-preview.png',
+  //       width: 752,
+  //       height: 400,
+  //       alt: 'portfolio thumbnail',
+  //     },
+  //   ],
+  // },
+};
+// const radioCanada = Radio_Canada({
+//   subsets: ['latin'],
+//   display: 'swap',
+// });
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en' className='something'>
+      <body>{children}</body>
+    </html>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 const config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-green': '#274C5B',
+        'secondary-green': '#577E65',
+        'third-green': '#EFF6F1',
+        'primary-yellow': '#EFD372',
+        'primary-grey': '#525C60',
+        'secondary-grey': '#D4D4D4',
+        'primary-white': '#F9F8F8',
+      },
+    },
   },
   plugins: [],
 };
