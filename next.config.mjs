510ed198fe340e6d0398d48ec2a5a436c37800b0
SpicyArepa <infantevladimir@gmail.com>
2023-04-06 10:20:40 -0400

    config ready: added linter rules, updated readme

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,15 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions, import/extensions
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env.mjs'));
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
@@ -14,9 +18,9 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ['en'],
+  //   defaultLocale: 'en',
+  // },
 };
 export default config;
