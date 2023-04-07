1a2199fd22539683195abdb13c0e11a7d54003d4
SpicyArepa <infantevladimir@gmail.com>
2023-04-07 13:59:18 -0400

    added folders for structure

---
+++ src/app/service/page.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Service: NextPage = () => {
+  return <div>page</div>;
+};
+
+export default Service;
---
+++ src/app/shop/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Product: NextPage = () => {
+  return <div>page</div>;
+};
+
+export default Product;
