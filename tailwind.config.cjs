03eb506ddb1d61161fd58b4a36b92017de493670
SpicyArepa <infantevladimir@gmail.com>
2023-04-13 15:57:05 -0400

    added radix ui for navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ const config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
-      yellotail: 'var(--font-yellowtail)',
+      yellowtail: 'var(--font-yellowtail)',
       roboto: 'var(--font-roboto)',
       openSans: 'var(--font-opensans)',
     },
@@ -17,9 +17,24 @@ const config = {
         'secondary-grey': '#D4D4D4',
         'primary-white': '#F9F8F8',
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line global-require
+  plugins: [require('tailwindcss-animate')],
 };
 
 module.exports = config;
